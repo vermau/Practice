6d75e7e0b7fe2053d212d60250ffc3f176ca12f6
Ujjwal Verma <vermau81@gmail.com>
2018-12-31 06:57:40 +1100

    Create groups of data inside a TableView

---
+++ TableGroupings/TableGroupings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableGroupings
+//
+//  Created by Ujjwal Verma on 30/12/18.
+//  Copyright © 2018 Ujjwal Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
