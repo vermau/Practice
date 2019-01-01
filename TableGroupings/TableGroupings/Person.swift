61c6e8e8ddd90f3ce059aa3e4ead3d78a92bab0a
Ujjwal Verma <vermau81@gmail.com>
2019-01-01 19:42:09 +1100

    A new struct named Person added to project

---
+++ TableGroupings/TableGroupings/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  TableGroupings
+//
+//  Created by Ujjwal Verma on 1/1/19.
+//  Copyright © 2019 Ujjwal Verma. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let age: Int
+}
