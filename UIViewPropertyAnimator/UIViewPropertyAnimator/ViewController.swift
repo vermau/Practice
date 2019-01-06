c3e09a8b830762e8ef125bd7061bdd6cafe0cf4a
Ujjwal Verma <vermau81@gmail.com>
2019-01-07 04:32:53 +1100

    Applied scaling and blur effect to the imageView in connection to the slider control

--- UIViewPropertyAnimator/UIViewPropertyAnimator/ViewController.swift
+++ UIViewPropertyAnimator/UIViewPropertyAnimator/ViewController.swift
@@ -11,18 +11,49 @@ import UIKit
 class ViewController: UIViewController {
     
     let imageView = UIImageView(image: #imageLiteral(resourceName: "girl"))
+    let slider = UISlider()
+    let animator = UIViewPropertyAnimator(duration: 0.5, curve: .linear, animations: nil)
+    let visualEffectView = UIVisualEffectView(effect: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupImageView()
         
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        setupImageView()
+        setupVisualBlurEffectView()
+        setupSlider()
+        setupTargets()
+        
+        animator.addAnimations {
+            self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+            self.visualEffectView.effect = UIBlurEffect(style: .light)
+        }
+    }
+    
+    @objc fileprivate func handleSliderChange(slider: UISlider) {
+        animator.fractionComplete = CGFloat(slider.value)
     }
     
     @objc fileprivate func handleTap() {
         UIView.animate(withDuration: 1.0) {
             self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
         }
+    }
+
+    fileprivate func setupVisualBlurEffectView() {
+        //let blurEffect = UIBlurEffect(style: .dark)
+        
+        view.addSubview(visualEffectView)
+        visualEffectView.fillSuperview()
+    }
+    
+    fileprivate func setupTargets() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        slider.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
+    }
+    
+    fileprivate func setupSlider() {
+        view.addSubview(slider)
+        slider.anchor(top: imageView.bottomAnchor, leading: imageView.leadingAnchor, bottom: nil, trailing: imageView.trailingAnchor)
     }
 
     fileprivate func setupImageView() {
