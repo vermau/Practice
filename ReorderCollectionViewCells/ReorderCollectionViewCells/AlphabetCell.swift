c3f90380babcca2840561d8430e02301625536c3
Ujjwal Verma <vermau81@gmail.com>
2019-01-15 03:41:49 +1100

    Initial Commit

---
+++ ReorderCollectionViewCells/ReorderCollectionViewCells/AlphabetCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlphabetCell.swift
+//  ReorderCollectionViewCells
+//
+//  Created by Ujjwal Verma on 15/1/19.
+//  Copyright © 2019 Ujjwal Verma. All rights reserved.
+//
+
+import UIKit
+
+class AlphabetCell: UICollectionViewCell {
+    
+    @IBOutlet weak var alphabetLabel: UILabel!
+}
